4700856e7c166e738ad705aeb0128f6b6f277109
Geoff Sutcliffe <2geoff@pobox.com>
2026-03-19 14:17:06 -0400

    new THF, new author

--- Examples/THF_Finite.p
+++ Examples/THF_Finite.p
@@ -2,20 +2,21 @@
 thf(beverage_type,type,   beverage: $tType ).
 thf(syrup_type,type,      syrup: $tType ).
 thf(coffee_decl,type,     coffee: beverage ).
-thf(mix_decl,type,        mix: beverage > syrup > beverage ).
+thf(vanilla_decl,type,    vanilla: syrup ).
+thf(mix_decl,type,        mix: ( beverage > syrup ) > beverage ).
 thf(heat_decl,type,       heat: beverage > beverage ).
-thf(heated_mix_decl,type, heated_mix: beverage > syrup > beverage ).
+thf(heated_mix_decl,type, heated_mix: ( beverage > syrup ) > beverage ).
 thf(hot_decl,type,        hot: beverage > $o ).
 
 thf(heated_mix,axiom,
     ( heated_mix
-    = ( ^ [B: beverage,S: syrup] : ( heat @ ( mix @ B @ S ) ) ) ) ).
+    = ( ^ [F: beverage > syrup] : ( heat @ ( mix @ F ) ) ) ) ).
 
 thf(hot_mixture,axiom,
-    ! [B: beverage,S: syrup] : ( hot @ ( heated_mix @ B @ S ) ) ).
+    ! [F: beverage > syrup] : ( hot @ ( heated_mix @ F ) ) ).
 
 thf(heated_coffee_mix,axiom,
-    ! [S: syrup] : ( ( heated_mix @ coffee @ S ) = coffee ) ).
+    ! [F: beverage > syrup] : ( ( heated_mix @ F ) = coffee ) ).
 
 thf(hot_coffee,conjecture,
     ? [Mixture: syrup > beverage] :
--- Examples/THF_Finite.s
+++ Examples/THF_Finite.s
@@ -1,33 +1,36 @@
 %------------------------------------------------------------------------------
-thf(beverage_decl,type,   beverage: $tType ).
-thf(syrup_decl,type,      syrup: $tType ).
-thf(coffee_type,type,     coffee: beverage ).
-thf(mix_type,type,        mix: beverage > syrup > beverage ).
-thf(heat_type,type,       heat: beverage > beverage ).
-thf(heated_mix_type,type, heated_mix: beverage > syrup > beverage ).
-thf(hot_type,type,        hot: beverage > $o ).
+thf(beverage_type,type,   beverage: $tType ).
+thf(syrup_type,type,      syrup: $tType ).
+thf(coffee_decl,type,     coffee: beverage ).
+thf(vanilla_decl,type,    vanilla: syrup ).
+thf(mix_decl,type,        mix: ( beverage > syrup ) > beverage ).
+thf(heat_decl,type,       heat: beverage > beverage ).
+thf(heated_mix_decl,type, heated_mix: ( beverage > syrup ) > beverage ).
+thf(hot_decl,type,        hot: beverage > $o ).
 
-thf(d_beverage_decl,type, d_beverage: $tType ).
-thf(d_syrup_decl,type,    d_syrup: $tType ).
-thf(d2beverage_type,type, d2beverage: d_beverage > beverage ).
-thf(d2syrup_type,type,    d2syrup: d_syrup > syrup ).
-thf(d_coffee_type,type,   d_coffee: d_beverage ).
-thf(d_date_type,type,     d_date: d_syrup ).
+thf(d_beverage_type,type, d_beverage: $tType ).
+thf(d_syrup_type,type,    d_syrup: $tType ).
+thf(d2beverage_decl,type, d2beverage: d_beverage > beverage ).
+thf(d2syrup_decl,type,    d2syrup: d_syrup > syrup ).
+thf(d_coffee_decl,type,   d_coffee: d_beverage ).
+thf(d_vanilla_decl,type,  d_vanilla: d_syrup ).
 
 thf(hot_coffee,interpretation,
-    ( ( ! [B: beverage] : ? [DB: d_beverage] : ( B = ( d2beverage @ DB ) )
-      & ! [DB: d_beverage] : ( DB = d_coffee )
-      & ! [DB1: d_beverage,DB2: d_beverage] :
-          ( ( ( d2beverage @ DB1 ) = ( d2beverage @ DB2 ) ) => ( DB1 = DB2 ) )
-      & ! [S: syrup] : ? [DS: d_syrup] : ( S = ( d2syrup @ DS ) )
-      & ! [DS: d_syrup] : ( DS = d_date )
-      & ! [DS1: d_syrup,DS2: d_syrup] :
-          ( ( ( d2syrup @ DS1 ) = ( d2syrup @ DS2 ) ) => ( DS1 = DS2 ) ) )
-    & ( ( ( mix @ ( d2beverage @ d_coffee ) @ ( d2syrup @ d_date ) ) 
-        = ( d2beverage @ d_coffee ) )
-      & ( ( heat @ ( d2beverage @ d_coffee ) ) 
-        = ( d2beverage @ d_coffee ) )
-      & ( ( heated_mix @ ( d2beverage @ d_coffee ) @ ( d2syrup @ d_date ) ) 
-        = ( d2beverage @ d_coffee ) )
-      & ( hot @ ( d2beverage @ d_coffee ) ) ) ) ).
+    ( ! [B: beverage] : ? [DB: d_beverage] : ( B = ( d2beverage @ DB ) )
+    & ! [DB: d_beverage] : ( DB = d_coffee )
+    & ! [DB1: d_beverage,DB2: d_beverage] :
+        ( ( ( d2beverage @ DB1 ) = ( d2beverage @ DB2 ) )
+       => ( DB1 = DB2 ) )
+    & ! [S: syrup] : ? [DS: d_syrup] : ( S = ( d2syrup @ DS ) )
+    & ! [DS: d_syrup] : ( DS = d_vanilla )
+    & ! [DS1: d_syrup,DS2: d_syrup] :
+        ( ( ( d2syrup @ DS1 ) = ( d2syrup @ DS2 ) )
+       => ( DS1 = DS2 ) )
+    & ( coffee = ( d2beverage @ d_coffee ) )
+    & ( vanilla = ( d2syrup @ d_vanilla ) )
+    & ( ( heat @ ( d2beverage @ d_coffee ) ) = d_coffee )
+    & ( mix = ( ^ [F: beverage > syrup] : ( d2beverage @ d_coffee ) ) )
+    & ( heated_mix
+      = ( ^ [F: beverage > syrup] : ( d2beverage @ d_coffee ) ) )
+    & ( hot @ ( d2beverage @ d_coffee ) ) ) ).
 %------------------------------------------------------------------------------
